579225d5efb67455a4df5c941321dae3b42fbfa7
fatimaezzahra <fatimaezzahramatlite@gmail.com>
2023-09-05 12:46:34 +0000

    my commit

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module creates a class named LockedClass"""
+
+
+class LockedClass:
+    """This class has no class or object attribute"""
+
+    __slots__ = ['first_name']
+
